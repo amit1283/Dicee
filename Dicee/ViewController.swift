dd0bdc78fec375bef4f9bef8323f74b74be9aaa8
Amit Rana <amit1283@icloud.com>
2017-10-16 00:13:14 +0530

    Roll Button work and changing Dice Image

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceImageView1: UIImageView!
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
@@ -25,7 +30,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onRollBtnPressed(_ sender: Any) {
+        print("button pressed")
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        print(randomDiceIndex1)
+        print(randomDiceIndex2)
+        print(diceArray[0])
         
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
